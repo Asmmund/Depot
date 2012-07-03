69e190d5a850f876f57dafdf27e2ca60ec58c0ea
Antony <antony.ermolenko@gmail.com>
2012-07-03 10:17:08 +0300

    yesterdays commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+  validates :title, :description, :image_url, presence:true
+  validates :price, numericality:{greater_than_or_equal_to: 0.01}
+  validates :title,uniqueness: true
+  validates :image_url,allow_blank: true,format: {
+    with: %r{\.(jpg|gif|png)$}i,
+    message: 'must be in accepted image format'
+  }
 end
