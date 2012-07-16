90f3720b382e0163d152799a35851c362a8f69e8
Asmmund <antony.ermolenko@gmail.com>
2012-07-16 11:35:39 +0300

    Added Rollback for items in a cart db migration

--- db/migrate/20120715161739_combine_items_in_cart.rb
+++ db/migrate/20120715161739_combine_items_in_cart.rb
@@ -18,5 +18,17 @@ class CombineItemsInCart < ActiveRecord::Migration
   end
 
   def down
+    # split items with quantity >1 into individual items
+    LineItem.where('quantity>1').each do |line_item|
+      # add individual items
+      line_item.quantity.times do
+        LineItem.create cart_id: line_item.cart_id,
+          product_id: line_item.product_id, quantity: 1
+        
+      end
+      # remove original item
+      line_item.destroy
+      
+    end
   end
 end
