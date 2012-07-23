ab3a32e79771b0e3d2bcc0680e9c54dba46e4ca9
Asmmund <antony.ermolenko@gmail.com>
2012-07-23 12:24:48 +0300

    ajax added, rake functional error

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -48,6 +48,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_url }
+        format.js { @current_item = @line_item }
         format.json { render json: @line_item, 
           status: :created, location: @line_item,
         status: :created, location: @line_item}
--- test/functional/line_items_controller_test.rb
+++ test/functional/line_items_controller_test.rb
@@ -21,7 +21,7 @@ class LineItemsControllerTest < ActionController::TestCase
       post :create, product_id: products(:ruby).id
     end
 
-    assert_redirected_to cart_path(assigns(:line_item).cart)
+    assert_redirected_to store_path
   end
 
   test "should show line_item" do
@@ -44,6 +44,19 @@ class LineItemsControllerTest < ActionController::TestCase
       delete :destroy, id: @line_item
     end
 
-    assert_redirected_to line_items_path
+    assert_redirected_to store_path
   end
+  
+  test 'should create line_item via ajax' do
+    assert_difference('LineItem.count') do
+      xhr :post, :create, product_id: products(:ruby).id
+    end
+    
+    assert_response :success
+    assert_select_jquery :html, '#cart' do
+      assert_select 'tr#current_item td', /Programming ruby 1.9/
+    end
+  end
+  
+  
 end
